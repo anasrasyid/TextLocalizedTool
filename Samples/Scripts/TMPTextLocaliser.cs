87e4bfb5b68618d831d189e13df4e0bbeb7fc2fe
Anas Rasyid <anasrasyid333@gmail.com>
2021-04-13 16:40:40 +0700

    Sample Localised

---
+++ Samples/Scripts/TMPTextLocaliser.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using TMPro;
+using personaltools.textlocalizedtool;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class TMPTextLocaliser : MonoBehaviour
+{
+    TextMeshProUGUI textField;
+
+    public LocalisedString localisedString;
+
+    void Start()
+    {
+        textField = GetComponent<TextMeshProUGUI>();
+        ShowText();
+        LocalizationManager.Instance.onChangeLanguage += ShowText;
+    }
+
+    public void ShowText()
+    {
+        textField.text = localisedString.Value;
+    }
+
+    private void OnDestroy()
+    {
+        LocalizationManager.Instance.onChangeLanguage -= ShowText;
+    }
+}
---
+++ Samples/Scripts/TextLocaliser.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using personaltools.textlocalizedtool;
+
+public class TextLocaliser : MonoBehaviour
+{
+    Text textField;
+
+    public LocalisedString localisedString;
+
+    void Start()
+    {
+        textField = GetComponent<Text>();
+        ShowText();
+        LocalizationManager.Instance.onChangeLanguage += ShowText;
+    }
+
+    public void ShowText()
+    {
+        textField.text = localisedString.Value;
+    }
+
+    private void OnDestroy()
+    {
+        LocalizationManager.Instance.onChangeLanguage -= ShowText;
+    }
+}
