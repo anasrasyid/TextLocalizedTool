9ec3cf02304dec4f26c49640452d241c82c44258
Anas Rasyid <anasrasyid333@gmail.com>
2021-04-28 18:38:51 +0700

    Add Languages

--- Runtime/Languages.cs
+++ Runtime/Languages.cs
@@ -1,19 +1,7 @@
 ﻿using System;
-using System.Reflection;
-using UnityEngine;
 
 namespace personaltools.textlocalizedtool
 {
-    public enum Languages 
-    {
-        [StringValue("en")]
-        English,
-        [StringValue("fr")]
-        France,
-        [StringValue("ind")]
-        Indonesia 
-    };
-
     [Serializable]
     public class StringValueAttribute : Attribute
     {
@@ -25,35 +13,336 @@ namespace personaltools.textlocalizedtool
         }
     }
 
-    public static class LanguagesExtensions
+    public enum Languages 
     {
-        public static string GetStringValue(this Languages value)
-        {
-            // Get the type
-            Type type = value.GetType();
+        [StringValue("sq")]
+        Albanian,
 
-            // Get fieldinfo for this type
-            FieldInfo fieldInfo = type.GetField(value.ToString());
+        [StringValue("am")]
+        Amharic,
 
-            // Get the stringvalue attributes
-            StringValueAttribute[] attribs = fieldInfo.GetCustomAttributes(
-                typeof(StringValueAttribute), false) as StringValueAttribute[];
+        [StringValue("ar")]
+        Arabic,
 
-            // Return the first if there was a match.
-            return attribs.Length > 0 ? attribs[0].StringValue : null;
-        }
+        [StringValue("hy")]
+        Armenian,
 
-        public static int GetLanguagesIndex(this string value)
-        {
-            int index = -1;
+        [StringValue("az")]
+        Azerbaijani,
 
-            foreach (Languages lan in (Languages[]) Enum.GetValues(typeof(Languages)))
-            {
-                if (value.Contains(lan.GetStringValue()))
-                    index = (int)lan;
-            }
+        [StringValue("eu")]
+        Basque,
 
-            return index;
-        }
-    }
+        [StringValue("be")]
+        Belarusian,
+
+        [StringValue("bn")]
+        Bengali,
+
+        [StringValue("bs")]
+        Bosnian,
+
+        [StringValue("bg")]
+        Bulgarian,
+
+        [StringValue("ca")]
+        Catalan,
+
+        [StringValue("ceb")]
+        Cebuano,
+
+        [StringValue("zh")]
+        Chinese_Simplified,
+
+        [StringValue("zh-TW")]
+        Chinese_Traditional,
+
+        [StringValue("co")]
+        Corsican,
+
+        [StringValue("hr")]
+        Croatian,
+
+        [StringValue("cs")]
+        Czech,
+
+        [StringValue("da")]
+        Danish,
+
+        [StringValue("nl")]
+        Dutch,
+
+        [StringValue("en")]
+        English,
+
+        [StringValue("eo")]
+        Esperanto,
+
+        [StringValue("et")]
+        Estonian,
+
+        [StringValue("fi")]
+        Finnish,
+
+        [StringValue("fr")]
+        French,
+
+        [StringValue("fy")]
+        Frisian,
+
+        [StringValue("gl")]
+        Galician,
+
+        [StringValue("ka")]
+        Georgian,
+
+        [StringValue("de")]
+        German,
+
+        [StringValue("el")]
+        Greek,
+
+        [StringValue("gu")]
+        Gujarati,
+
+        [StringValue("ht")]
+        Haitian_Creole,
+
+        [StringValue("ha")]
+        Hausa,
+
+        [StringValue("haw")]
+        Hawaiian,
+
+        [StringValue("he")]
+        Hebrew,
+
+        [StringValue("hi")]
+        Hindi,
+
+        [StringValue("hmn")]
+        Hmong,
+
+        [StringValue("hu")]
+        Hungarian,
+
+        [StringValue("is")]
+        Icelandic,
+
+        [StringValue("ig")]
+        Igbo,
+
+        [StringValue("id")]
+        Indonesian,
+
+        [StringValue("ga")]
+        Irish,
+
+        [StringValue("it")]
+        Italian,
+
+        [StringValue("ja")]
+        Japanese,
+
+        [StringValue("jv")]
+        Javanese,
+
+        [StringValue("kn")]
+        Kannada,
+
+        [StringValue("kk")]
+        Kazakh,
+
+        [StringValue("km")]
+        Khmer,
+
+        [StringValue("rw")]
+        Kinyarwanda,
+
+        [StringValue("ko")]
+        Korean,
+
+        [StringValue("ku")]
+        Kurdish,
+
+        [StringValue("ky")]
+        Kyrgyz,
+
+        [StringValue("lo")]
+        Lao,
+
+        [StringValue("la")]
+        Latin,
+
+        [StringValue("lv")]
+        Latvian,
+
+        [StringValue("lt")]
+        Lithuanian,
+
+        [StringValue("lb")]
+        Luxembourgish,
+
+        [StringValue("mk")]
+        Macedonian,
+
+        [StringValue("mg")]
+        Malagasy,
+
+        [StringValue("ms")]
+        Malay,
+
+        [StringValue("ml")]
+        Malayalam,
+
+        [StringValue("mt")]
+        Maltese,
+
+        [StringValue("mi")]
+        Maori,
+
+        [StringValue("mr")]
+        Marathi,
+
+        [StringValue("mn")]
+        Mongolian,
+
+        [StringValue("my")]
+        Myanmar_Burmese,
+
+        [StringValue("ne")]
+        Nepali,
+
+        [StringValue("no")]
+        Norwegian,
+
+        [StringValue("ny")]
+        Nyanja_Chichewa,
+
+        [StringValue("or")]
+        Odia_Oriya,
+
+        [StringValue("ps")]
+        Pashto,
+
+        [StringValue("fa")]
+        Persian,
+
+        [StringValue("pl")]
+        Polish,
+
+        [StringValue("pt")]
+        Portuguese_Portugal,
+
+        [StringValue("pt")]
+        Brazil,
+
+        [StringValue("pa")]
+        Punjabi,
+
+        [StringValue("ro")]
+        Romanian,
+
+        [StringValue("ru")]
+        Russian,
+
+        [StringValue("sm")]
+        Samoan,
+
+        [StringValue("gd")]
+        Scots_Gaelic,
+
+        [StringValue("sr")]
+        Serbian,
+
+        [StringValue("st")]
+        Sesotho,
+
+        [StringValue("sn")]
+        Shona,
+
+        [StringValue("sd")]
+        Sindhi,
+
+        [StringValue("si")]
+        Sinhala_Sinhalese,
+
+        [StringValue("sk")]
+        Slovak,
+
+        [StringValue("sl")]
+        Slovenian,
+
+        [StringValue("so")]
+        Somali,
+
+        [StringValue("es")]
+        Spanish,
+
+        [StringValue("su")]
+        Sundanese,
+
+        [StringValue("sw")]
+        Swahili,
+
+        [StringValue("sv")]
+        Swedish,
+
+        [StringValue("tl")]
+        Tagalog_Filipino,
+
+        [StringValue("tg")]
+        Tajik,
+
+        [StringValue("ta")]
+        Tamil,
+
+        [StringValue("tt")]
+        Tatar,
+
+        [StringValue("te")]
+        Telugu,
+
+        [StringValue("th")]
+        Thai,
+
+        [StringValue("tr")]
+        Turkish,
+
+        [StringValue("tk")]
+        Turkmen,
+
+        [StringValue("uk")]
+        Ukrainian,
+
+        [StringValue("ur")]
+        Urdu,
+
+        [StringValue("ug")]
+        Uyghur,
+
+        [StringValue("uz")]
+        Uzbek,
+
+        [StringValue("vi")]
+        Vietnamese,
+
+        [StringValue("cy")]
+        Welsh,
+
+        [StringValue("xh")]
+        Xhosa,
+
+        [StringValue("yi")]
+        Yiddish,
+
+        [StringValue("yo")]
+        Yoruba,
+
+        [StringValue("zu")]
+        Zulu,
+
+        [StringValue("cst")]
+        Custom_Language
+    };
 }
---
+++ Runtime/LanguagesExtensions.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Reflection;
+
+namespace personaltools.textlocalizedtool
+{
+    public static class LanguagesExtensions
+    {
+        public static string GetStringValuesAtribute(this Languages value)
+        {
+            // Get the type
+            Type type = value.GetType();
+
+            // Get fieldinfo for this type
+            FieldInfo fieldInfo = type.GetField(value.ToString());
+
+            // Get the stringvalue attributes
+            StringValueAttribute[] attribs = fieldInfo.GetCustomAttributes(
+                typeof(StringValueAttribute), false) as StringValueAttribute[];
+
+            // Return the first if there was a match.
+            return attribs.Length > 0 ? attribs[0].StringValue : null;
+        }
+
+        public static Languages GetLanguageEnumFromCode(this string value)
+        {
+            foreach (Languages lan in (Languages[])Enum.GetValues(typeof(Languages)))
+            {
+                if (value.CompareTo(lan.GetStringValuesAtribute()) == 0)
+                    return lan;
+            }
+            return Languages.Custom_Language;
+        }
+
+        public static Languages GetLanguageEnum(this string value)
+        {
+            foreach (Languages lan in (Languages[])Enum.GetValues(typeof(Languages)))
+            {
+                if (value.CompareTo(lan.ToString()) == 0)
+                    return lan;
+            }
+            return Languages.Custom_Language;
+        }
+
+        public static int GetLanguagesIndex(this string value)
+        {
+            foreach (Languages lan in (Languages[])Enum.GetValues(typeof(Languages)))
+            {
+                if (value.CompareTo(lan.ToString()) == 0)
+                    return (int)lan;
+            }
+
+            return -1;
+        }
+
+        public static int GetLanguagesIndexFromCode(this string value)
+        {
+            foreach (Languages lan in (Languages[])Enum.GetValues(typeof(Languages)))
+            {
+                if (value.CompareTo(lan.GetStringValuesAtribute()) == 0)
+                {
+                    return (int)lan;
+                }                    
+            }
+            return -1;
+        }
+    }
+}
